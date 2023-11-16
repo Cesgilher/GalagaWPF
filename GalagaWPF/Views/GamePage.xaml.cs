f2c6451c26cd054df0fe98959a4af9a1df39defa
César Hernando Hernando <cehernando@es.gestamp.com>
2023-11-16 22:17:31 +0100

    88%

--- GalagaWPF/Views/GamePage.xaml.cs
+++ GalagaWPF/Views/GamePage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace GalagaWPF
 {
@@ -22,16 +23,187 @@ namespace GalagaWPF
     public partial class GamePage : Window
     {
         Menu menu;
+
+        bool goLeft, goRight;
+
+        List<Rectangle> itemsToRemove = new List<Rectangle>();
+
+        int enemyImages = 0;
+        int bulletTimer = 0;
+        int bulletTimerLimit = 90;
+        int totalEnemies = 0;
+        int enemySpeed = 6;
+        bool gameOver = false;
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        ImageBrush playerSkin = new ImageBrush();
         public GamePage(Menu menu)
         {
             InitializeComponent();
             
             this.menu = menu;
             this.Show();
-            
-           
+
+            gameTimer.Tick += GameLoop;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Start();
+            playerSkin.ImageSource = new BitmapImage(new Uri("../../../Resources/player.png", UriKind.Relative));
+            player.Fill = playerSkin;
+
+            myCanvas.Focus();
+
+
         }
 
+        
+
+        
+
+        private void GameLoop(object? sender, EventArgs e)
+        {
+
+            if (goLeft == true && Canvas.GetLeft(player) > 0)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) - 10);
+            }
+            if (goRight == true && Canvas.GetLeft(player) + 80 < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) + 10);
+            }
+
+            bulletTimer -= 3;
+
+            if (bulletTimer < 0)
+            {
+                EnemyBulletMaker(Canvas.GetLeft(player) + 20, 10);
+                bulletTimer = bulletTimerLimit;
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                goLeft = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                goRight = true;
+            }
+            
+        }
+
+
+
+        
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                goLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                goRight = false;
+            }
+            if (e.Key == Key.Space)
+            {
+                Rectangle newBullet = new Rectangle
+                {
+                    Tag = "bullet",
+                    Height = 20,
+                    Width = 5,
+                    Fill = Brushes.White,
+                    Stroke = Brushes.Red,
+                };
+
+                Canvas.SetTop(newBullet, Canvas.GetTop(player) - newBullet.Height);
+                Canvas.SetLeft(newBullet, Canvas.GetLeft(player) + player.Width / 2);
+
+                myCanvas.Children.Add(newBullet);
+            }
+        }
+
+        private void EnemyBulletMaker(double x, double y)
+        {
+            Rectangle enemyBullet = new Rectangle
+            {
+                Tag = "enemyBullet",
+                Height = 40,
+                Width = 15,
+                Fill = Brushes.Yellow,
+                Stroke = Brushes.Black,
+                StrokeThickness = 5,
+
+            };
+
+            Canvas.SetTop(enemyBullet, y);
+            Canvas.SetLeft(enemyBullet, x);
+
+            myCanvas.Children.Add(enemyBullet);
+
+
+        }
+
+        private void MakeEnemies(int limit)
+        {
+            int left = 0;
+
+            totalEnemies = limit;
+
+            for (int i = 0; i < limit; i++)
+            {
+                ImageBrush enemySkin = new ImageBrush();
+
+                Rectangle newEnemy = new Rectangle
+                {
+                    Tag = "enemy",
+                    Height = 45,
+                    Width = 45,
+                    Fill = enemySkin,
+                };
+
+                Canvas.SetTop(newEnemy, 10); 
+                Canvas.SetLeft(newEnemy, left);
+                myCanvas.Children.Add(newEnemy);
+                left -= 60;
+                enemyImages++;
+                if (enemyImages > 8)
+                {
+                    enemyImages = 1;
+                }
+                switch (enemyImages)
+                {
+                    case 1:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader1.gif", UriKind.Relative));
+                        break;
+                    case 2:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader2.gif", UriKind.Relative));
+                        break;
+                    case 3:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader3.gif", UriKind.Relative));
+                        break;
+                    case 4:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader4.gif", UriKind.Relative));
+                        break;
+                    case 5:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader5.gif", UriKind.Relative));
+                        break;
+                    case 6:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader6.gif", UriKind.Relative));
+                        break;
+                    case 7:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader7.gif", UriKind.Relative));
+                        break;
+                    case 8:
+                        enemySkin.ImageSource = new BitmapImage(new Uri("../../../images/invader8.gif", UriKind.Relative));
+                        break;
+                }
+            }
+        }
+
+        private void ShowGameOverScreen(string msg) { }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
@@ -40,10 +212,12 @@ namespace GalagaWPF
                 if (escapePanel.Visibility == Visibility.Collapsed)
                 {
                     escapePanel.Visibility = Visibility.Visible;
+                    myCanvas.Visibility = Visibility.Collapsed;
                 }
                 else
                 {
                     escapePanel.Visibility = Visibility.Collapsed;
+                    myCanvas.Visibility = Visibility.Visible;
                 }
             }
         }
